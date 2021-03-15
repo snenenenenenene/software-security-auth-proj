fbe57f256914335d89052f11781a5e26e5c2e1e8
Robin Proost <robin-proost@hotmail.com>
2021-03-15 15:01:10 +0100

    changed procfile

--- app.js
+++ app.js
@@ -54,5 +54,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000); 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 module.exports = app; 
